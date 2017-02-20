ac4872db7e0c7bfb6e3c120d9e17659c8983764a
WangShanpeng <wangshanpeng169@163.com>
2017-02-20 20:33:09 +0800

    修改为Python3拨号脚本

--- buaaNet/buaaNet/buaaNet.py
+++ buaaNet/buaaNet/buaaNet.py
@@ -1,26 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import urllib
-import urllib2
+import urllib.request
+import urllib.error
+import urllib.parse
 import base64
 import time
+
 class login:
     def __init__(self,Username,Password):
         self.Url = 'https://gw.buaa.edu.cn:801/include/auth_action.php'
-        Password = base64.b64encode(Password)
-        Password = urllib.quote(Password)
+        Password = base64.b64encode(Password.encode(encoding='utf-8'))
+        Password = urllib.parse.quote(Password)
         self.Data = 'action=login&username='+Username+'&password={B}'+Password+'&ac_id=22&user_ip=&nas_ip=&user_mac=&save_me=1&ajax=1'
+        self.Data = self.Data.encode(encoding='utf-8')
     def auto_login(self):
-        request = urllib2.Request(url=self.Url,data=self.Data)
-        response = urllib2.urlopen(request)
-        print response.read()
+        request_data = urllib.request.Request(url=self.Url,data=self.Data)
+        response_data = urllib.request.urlopen(request_data)
+        print (str(response_data.read(),encoding='utf-8'))
         
 #####################################################
 def internet_on():
     try:
-        urllib2.urlopen('https://www.baidu.com/', timeout=1)
+        urllib.request.urlopen('https://www.baidu.com/', timeout=1)
         return True
-    except urllib2.URLError as err: 
+    except urllib.error.URLError as err:
         return False
 
 #####################################################
@@ -29,7 +32,5 @@ if __name__ == '__main__':
         if not internet_on():
             Buaa = login(Username='by222222',Password='111111') # 111111替换为密码，by222222替换为学号
             Buaa.auto_login()
-            print "user login"
+            print ('User login!')
         time.sleep(60*2)
-  
-
