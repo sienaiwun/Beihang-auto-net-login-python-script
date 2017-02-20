7623ff64e06714846e20cf7c53331fe3ee0206b4
WangShanpeng <wangshanpeng169@163.com>
2017-02-20 22:57:22 +0800

    模拟Win10 Chrome登录；删掉了auto_login；添加了Response打印功能

--- buaaNet/buaaNet/buaaNet.py
+++ buaaNet/buaaNet/buaaNet.py
@@ -13,10 +13,14 @@ class login:
         Password = urllib.parse.quote(Password)
         self.Data = 'action=login&username='+Username+'&password={B}'+Password+'&ac_id=22&user_ip=&nas_ip=&user_mac=&save_me=1&ajax=1'
         self.Data = self.Data.encode(encoding='utf-8')
-    def auto_login(self):
-        request_data = urllib.request.Request(url=self.Url,data=self.Data)
-        response_data = urllib.request.urlopen(request_data)
-        print (str(response_data.read(),encoding='utf-8'))
+        req = urllib.request.Request(url=self.Url,data=self.Data)
+        req.add_header('User-Agent','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36')
+        with urllib.request.urlopen(req) as f:
+            print('User: ' + Username.upper() + ' login succeed!')
+            print('Status:', f.status, f.reason)
+            for k, v in f.getheaders():
+                print('%s: %s' % (k, v))
+            print('Data: ' + f.read().decode('utf-8'))
         
 #####################################################
 def internet_on():
@@ -30,7 +34,5 @@ def internet_on():
 if __name__ == '__main__':
     while True:
         if not internet_on():
-            Buaa = login(Username='by222222',Password='111111') # 111111替换为密码，by222222替换为学号
-            Buaa.auto_login()
-            print ('User login!')
-        time.sleep(60*2)
+            Buaa = login(Username='by222222', Password='111111') # 111111替换为密码，by222222替换为学号
+        time.sleep(60)
